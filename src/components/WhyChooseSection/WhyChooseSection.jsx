48d4b8ae4ec10e1eb5a8ef9f0779ecc10aa9900b
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-08-24 09:24:49 -0300

    refactor: add styles to images that were being called in the system

--- src/components/WhyChooseSection/WhyChooseSection.jsx
+++ src/components/WhyChooseSection/WhyChooseSection.jsx
@@ -9,8 +9,8 @@ function WhyChooseSection() {
             <div className={styles.container_all}>
                 <div className={styles.container}>
                     <div className={styles.imageContainer}>
-                        <img className={styles.ImgSistemas} src="/src/assets/Sistema.png" alt="" />
-                        <img className={styles.ImgInvests} src="/src/assets/Invest.png" alt="" />
+                        <img className={styles.ImgSistema} src="/src/assets/Sistema.png" alt="" />
+                        <img className={styles.ImgInvest} src="/src/assets/Invest.png" alt="" />
                     </div>
                     <div className={styles.container_Choose}>
                         <p> <FaCheck style={{ color: 'green' }} /> Task Automation </p>
