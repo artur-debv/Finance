5bc5de43ec06266e313a55e661d5d96dedff8908
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-08-24 14:29:14 -0300

    updates

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,8 +1,19 @@
 
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+  } from "@/components/ui/carousel"
+  
+
 import styles from './Features.module.css';
 function Features() {
     return (
-        <h1 className={styles.Title}>Features</h1>
+        <>
+            <h1 className={styles.Title}>Features</h1>
+        </>
     )
 }
 
