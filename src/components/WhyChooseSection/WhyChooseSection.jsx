b2e8659c139e1dd1a6eb272b61e1cad4766bace4
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-08-24 09:10:37 -0300

    updates

--- src/components/WhyChooseSection/WhyChooseSection.jsx
+++ src/components/WhyChooseSection/WhyChooseSection.jsx
@@ -9,6 +9,8 @@ function WhyChooseSection() {
             <div className={styles.container_all}>
                 <div className={styles.container}>
                     <div className={styles.imageContainer}>
+                        <img src="/src/assets/Sistema.png" alt="" />
+                        <img src="/src/assets/Invest.png" alt="" />
                     </div>
                     <div className={styles.container_Choose}>
                         <p> <FaCheck style={{ color: 'green' }} /> Task Automation </p>
